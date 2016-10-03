3fa980a4067f2e2285702108013022bb99248588
TeisD <teis.degreve@gmail.com>
2016-10-03 15:14:44 +0200

    Passwords didn't save when registering

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'role_id'];
+    protected $fillable = ['name', 'email', 'password', 'role_id'];
 
     /**
      * The attributes excluded from the model's JSON form.
