b49a07dfacd8490537146502152c0c28891d92b3
TeisD <teis.degreve@gmail.com>
2016-11-10 17:42:59 +0100

    Speed up page load

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,8 +10,15 @@
     <link rel="preload" href="/css/foundation-icons.min.css" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="//cdn.quilljs.com/0.20.1/quill.snow.css" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="/css/foundation-datepicker.min.css" as="style" onload="this.rel='stylesheet'">
-    <script src="/js/loadCSS.js"></script>
-    <script src="/js/cssrelpreload.js"></script>
+
+    {{-- js/loadCSS.js --}}
+    <script type="text/javascript">
+      !function(e){"use strict";var n=function(n,t,o){function i(e){return a.body?e():void setTimeout(function(){i(e)})}function r(){l.addEventListener&&l.removeEventListener("load",r),l.media=o||"all"}var d,a=e.document,l=a.createElement("link");if(t)d=t;else{var s=(a.body||a.getElementsByTagName("head")[0]).childNodes;d=s[s.length-1]}var f=a.styleSheets;l.rel="stylesheet",l.href=n,l.media="only x",i(function(){d.parentNode.insertBefore(l,t?d:d.nextSibling)});var u=function(e){for(var n=l.href,t=f.length;t--;)if(f[t].href===n)return e();setTimeout(function(){u(e)})};return l.addEventListener&&l.addEventListener("load",r),l.onloadcssdefined=u,u(r),l};"undefined"!=typeof exports?exports.loadCSS=n:e.loadCSS=n}("undefined"!=typeof global?global:this);
+    </script>
+    {{-- js/cssrelpreload.js --}}
+    <script type="text/javascript">
+        !function(t){if(t.loadCSS){var e=loadCSS.relpreload={};if(e.support=function(){try{return t.document.createElement("link").relList.supports("preload")}catch(e){return!1}},e.poly=function(){for(var e=t.document.getElementsByTagName("link"),n=0;n<e.length;n++){var r=e[n];"preload"===r.rel&&"style"===r.getAttribute("as")&&(t.loadCSS(r.href,r),r.rel=null)}},!e.support()){e.poly();var n=t.setInterval(e.poly,300);t.addEventListener&&t.addEventListener("load",function(){t.clearInterval(n)}),t.attachEvent&&t.attachEvent("onload",function(){t.clearInterval(n)})}}}(this);
+    </script>
 
     {{-- STYLESHEETS --}}
     <link rel="stylesheet" href="/css/normalize.min.css">
@@ -20,7 +27,6 @@
 
     {{-- SCRIPTS --}}
     <script src="/js/vendor/modernizr.js"></script>
-    <script src="/js/vendor/jquery.min.js"></script>
   </head>
 	<body id="wrapper">
         {{-- JS: Google Analytics --}}
@@ -116,6 +122,7 @@
 
         {{-- SCRIPTS --}}
         {{-- <script src="/js/foundation.min.js"></script> --}}
+        <script src="/js/vendor/jquery.min.js"></script>
         <script>
             $.ajaxSetup({
                 headers: {
